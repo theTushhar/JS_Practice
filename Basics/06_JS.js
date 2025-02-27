4ceb9fafeeeba750a14d51bc1a932b847d08c34b
Tushar Gupta <thetushara1@gmail.com>
2025-02-27 23:07:19 +0530

    New Files Added

---
+++ Basics/06_JS.js
@@ -0,0 +1,22 @@
+// Basic Comparisons both are same data types
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Compare the same datatype otherwise it will give unpredictable output
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
