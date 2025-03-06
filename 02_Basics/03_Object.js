395ebea2850d901a24d59c1060e16b746511b83a
Tushar Gupta <thetushara1@gmail.com>
2025-03-06 20:23:25 +0530

    Files Updates

--- 02_Basics/03_Object.js
+++ 02_Basics/03_Object.js
@@ -1,37 +1,52 @@
-// singleton
-// Object.create
+// Will cover these below tpics in later sessions
+// singleton // Object.create
+// We can create objects by two types - By Object literals & By Object.create
 
-// object literals
+// By object literals
 
+// Creating a Symbol to use as a unique key in the object
 const mySym = Symbol("key1");
 
+// Defining an object using object literal syntax
 const JsUser = {
-  name: "tushar",
-  "full name": "tushar gupta",
-  [mySym]: "mykey1",
-  age: 18,
-  location: "dehradun",
-  email: "tushar@google.com",
-  isLoggedIn: false,
-  lastLoginDays: ["Monday", "Saturday"],
+  name: "tushar", // String property
+  "full name": "tushar gupta", // String property with a space in the key name
+  [mySym]: "mykey1", // Using a Symbol as a key (enclosed in square brackets)
+  age: 18, // Number property
+  location: "dehradun", // String property
+  email: "tushar@google.com", // String property
+  isLoggedIn: false, // Boolean property
+  lastLoginDays: ["Monday", "Saturday"], // Array property
 };
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// Accessing object properties
+// console.log(JsUser.email) // Accessing using dot notation
+// console.log(JsUser["email"]) // Accessing using bracket notation
+// console.log(JsUser["full name"]) // Accessing a key with spaces
+// console.log(JsUser[mySym]) // Accessing a Symbol key
 
+//Recommended to access the property by the square brackets([]) not with the dot(.)
+
+// Modifying a property in the object
 JsUser.email = "tushar@chatgpt.com";
-// Object.freeze(JsUser)
-JsUser.email = "tushar@microsoft.com";
-// console.log(JsUser);
 
+// Freezing the object to prevent further modifications
+// Object.freeze(JsUser)
+
+// Attempting to modify a property after freezing (will not work if uncommented)
+JsUser.email = "tushar@microsoft.com";
+// console.log(JsUser); // Logging the object to check changes
+
+// Adding a method to the object
 JsUser.greeting = function () {
   console.log("Hello JS user");
 };
+
+// Adding another method that uses the object's properties
 JsUser.greetingTwo = function () {
-  console.log(`Hello JS user, ${this.name}`);
+  console.log(`Hello JS user, ${this.name}`); // Using 'this' to refer to the object's properties
 };
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// Calling the methods and logging their output
+console.log(JsUser.greeting()); // Logs "Hello JS user" and returns undefined (no explicit return)
+console.log(JsUser.greetingTwo()); // Logs "Hello JS user, tushar" and returns undefined (no explicit return)
--- 02_Basics/04_Object.js
+++ 02_Basics/04_Object.js
@@ -1,34 +1,53 @@
-// const tinderUser = new Object()
-const tinderUser = {};
+// A singleton object is an object that is instantiated only once
+// throughout the entire application. Even if you try to create it
+// multiple times, it will always refer back to the same instance.
 
-tinderUser.id = "123abc";
-tinderUser.name = "Sammy";
-tinderUser.isLoggedIn = false;
+// Creating an empty object using object literal syntax
+// const facebookUser = new Object() // Alternate way to create an object
+const facebookUser = {}; // Creating an empty object
 
-// console.log(tinderUser);
+// Adding properties to the facebookUser object
+facebookUser.id = "123abc";
+facebookUser.name = "Sammy";
+facebookUser.isLoggedIn = false;
 
+// console.log(facebookUser); // Logging the facebookUser object
+
+// Creating a nested object structure
 const regularUser = {
-  email: "some@gmail.com",
+  email: "some@gmail.com", // Top-level property
   fullname: {
     userfullname: {
-      firstname: "tushar",
-      lastname: "gupta",
+      firstname: "tushar", // Nested property
+      lastname: "gupta", // Nested property
     },
   },
 };
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// Accessing nested properties using dot notation
+// console.log(regularUser.fullname.userfullname.firstname); // Logs "tushar"
 
+// Optional chaining to safely access nested properties
+// If `fullname` is undefined or null, it won't throw an error
+// console.log(regularUser.fullname?.userfullname.firstname); // Logs "tushar"
+
+// Creating multiple objects
 const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" };
 const obj4 = { 5: "a", 6: "b" };
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// Combining objects into a single object (incorrect way, creates nested objects)
+// const obj3 = { obj1, obj2 } // Results in { obj1: { ... }, obj2: { ... } }
 
-const obj3 = { ...obj1, ...obj2 };
-// console.log(obj3);
+// Combining objects using Object.assign()
+// Copies properties from source objects (obj1, obj2, obj4) to the target object ({})
+// const obj3 = Object.assign({}, obj1, obj2, obj4); // Results in { 1: "a", 2: "b", 3: "a", 4: "b", 5: "a", 6: "b" }
 
+// Combining objects using the spread operator (modern and preferred way)
+const obj3 = { ...obj1, ...obj2 }; // Results in { 1: "a", 2: "b", 3: "a", 4: "b" }
+// console.log(obj3); // Logging the combined object
+
+// Array of user objects
 const users = [
   {
     id: 1,
@@ -44,32 +63,46 @@ const users = [
   },
 ];
 
-users[1].email;
-// console.log(tinderUser);
+// Accessing a property from an object inside an array
+users[1].email; // Accessing the email of the second user in the array
+// console.log(facebookUser); // Logging the facebookUser object
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// Getting keys, values, and entries of an object
+// console.log(Object.keys(facebookUser)); // Logs ["id", "name", "isLoggedIn"]
+// console.log(Object.values(facebookUser)); // Logs ["123abc", "Sammy", false]
+// console.log(Object.entries(facebookUser)); // Logs [["id", "123abc"], ["name", "Sammy"], ["isLoggedIn", false]]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// Checking if an object has a specific property
+// console.log(facebookUser.hasOwnProperty('isLoggedIn')); // Logs true
 
+// Creating a course object
 const course = {
   coursename: "js in english",
   price: "999",
   courseInstructor: "tushar",
 };
 
-// course.courseInstructor
+// Destructuring in JavaScript is a convenient way to extract
+// values from arrays or properties from objects and assign them
+// to variables in a single statement. It makes your code cleaner
+// and easier to read by unpacking data quickly.
 
-const { courseInstructor: instructor } = course;
+// Destructuring the course object to extract specific properties
+const { courseInstructor: instructor } = course; // Renaming `courseInstructor` to `instructor`
 
-// console.log(courseInstructor);
-console.log(instructor);
+// console.log(courseInstructor); // This would throw an error because `courseInstructor` is renamed to `instructor`
+console.log(instructor); // Logs "tushar"
 
+// Example of a JSON-like structure
 // {
 //     "name": "tushar",
 //     "coursename": "js in english",
 //     "price": "free"
 // }
 
-[{}, {}, {}];
+// Website for API looks Like gives result in JSON
+// https://randomuser.me/
+// https://randomuser.me/api/
+
+// Example of an array of objects (common in APIs)
+// [{}, {}, {}]
